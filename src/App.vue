6f0ece550d2905dfbb5d4f741781517b48e5e46a
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-06-14 15:39:26 +0100

    Add components to app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
+import AddTransactionForm from '@/components/AddTransactionForm.vue'
+import Chart from '@/components/Chart.vue'
+import TransactionHistory from './components/TransactionHistory.vue';
 </script>
 <template>
   <NavBar />
+  <main class="flex-1 p-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <AddTransactionForm />
+      <Chart />
+    </div>
+    <TransactionHistory />
+  </main>
 </template>
