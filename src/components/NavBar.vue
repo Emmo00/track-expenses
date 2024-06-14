911e693ad1458ba356bc7410c24b696431524660
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-06-14 15:36:04 +0100

    Navigation bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="bg-gray-900 text-white py-4 px-6">
+        <div class="flex items-center justify-between">
+            <h1 class="text-2xl font-bold">Personal Finance Tracker</h1>
+        </div>
+    </header>
+</template>
