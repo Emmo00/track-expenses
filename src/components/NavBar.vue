0a0dcbe58c030154d03f802a04d69b14f385ca14
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-06-14 15:45:12 +0100

    make theme consistent

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-gray-900 text-white py-4 px-6">
+    <header class="bg-black text-white py-4 px-6">
         <h1 class="text-2xl font-bold">Personal Finance Tracker</h1>
     </header>
 </template>
